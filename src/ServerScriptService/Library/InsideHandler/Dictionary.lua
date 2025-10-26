303b0174adf485d54c6a32caa8e277499bb22f63
Usman Muhammad <68919810+musman65@users.noreply.github.com>
2025-10-26 11:44:57 -0400

    Create Dictionary.lua
    init commit

---
+++ src/ServerScriptService/Library/InsideHandler/Dictionary.lua
@@ -0,0 +1,19 @@
+--Module Script
+local Dictionary = 
+{
+		["Stunned"] = 
+		{
+			
+		},
+		["Blocking"] = 
+		{
+				
+		},
+		["Busy"] = --To see if the person is already doing a move
+		{
+			
+		}
+			
+}
+
+return Dictionary
